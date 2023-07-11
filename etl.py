283eb46cf7a8a9255f07b0fdcf4daf3c56d3c9f0
Ernesto <48799528+ernestod10@users.noreply.github.com>
2023-07-11 19:34:11 -0400

    Update etl.py

--- etl.py
+++ etl.py
@@ -34,88 +34,88 @@ def execute_dest_query(query, data):
 
 #Extraccion
 query_estado = "SELECT * FROM ESTADO;"
-estado_results = execute_query(query_estado)
+estado_results = execute_source_query(query_estado)
 
 query_ciudad = "SELECT * FROM CIUDAD;"
-ciudad_results = execute_query(query_ciudad)
+ciudad_results = execute_source_query(query_ciudad)
 
 
 query_tipo_provee = "SELECT * FROM TIPO_PROVEE;"
-tipo_provee_results = execute_query(query_tipo_provee)
+tipo_provee_results = execute_source_query(query_tipo_provee)
 
 
 query_provee = "SELECT * FROM PROVEE;"
-provee_results = execute_query(query_provee)
+provee_results = execute_source_query(query_provee)
 
 query_especialidad = "SELECT * FROM ESPECIALIDAD;"
-especialidad_results = execute_query(query_especialidad)
+especialidad_results = execute_source_query(query_especialidad)
 
 query_sx = "SELECT * FROM SX;"
-sx_results = execute_query(query_sx)
+sx_results = execute_source_query(query_sx)
 
 
 query_estado_civil = "SELECT * FROM ESTADO_CIVIL;"
-estado_civil_results = execute_query(query_estado_civil)
+estado_civil_results = execute_source_query(query_estado_civil)
 
 query_medico = "SELECT * FROM MEDICO;"
-medico_results = execute_query(query_medico)
+medico_results = execute_source_query(query_medico)
 
 
 query_tiene = "SELECT * FROM TIENE;"
-tiene_results = execute_query(query_tiene)
+tiene_results = execute_source_query(query_tiene)
 
 
 query_area = "SELECT * FROM AREA;"
-area_results = execute_query(query_area)
+area_results = execute_source_query(query_area)
 
 
 query_medicina = "SELECT * FROM MEDICINA;"
-medicina_results = execute_query(query_medicina)
+medicina_results = execute_source_query(query_medicina)
 
 query_trabaja = "SELECT * FROM TRABAJA;"
-trabaja_results = execute_query(query_trabaja)
+trabaja_results = execute_source_query(query_trabaja)
 
 
 query_personal_sanitario = "SELECT * FROM PERSONAL_SANITARIO;"
-personal_sanitario_results = execute_query(query_personal_sanitario)
+personal_sanitario_results = execute_source_query(query_personal_sanitario)
 
 query_diagn = "SELECT * FROM DIAGN;"
-diagn_results = execute_query(query_diagn)
+diagn_results = execute_source_query(query_diagn)
 
 query_cama = "SELECT * FROM CAMA;"
-cama_results = execute_query(query_cama)
+cama_results = execute_source_query(query_cama)
 
 
 query_tratamiento = "SELECT * FROM TRATAMIENTO;"
-tratamiento_results = execute_query(query_tratamiento)
+tratamiento_results = execute_source_query(query_tratamiento)
 
 
 query_aplica = "SELECT * FROM APLICA;"
-aplica_results = execute_query(query_aplica)
+aplica_results = execute_source_query(query_aplica)
 
 
 query_pac = "SELECT * FROM PAC;"
-pac_results = execute_query(query_pac)
+pac_results = execute_source_query(query_pac)
 
 
 query_intervencion = "SELECT * FROM INTERVENCION;"
-intervencion_results = execute_query(query_intervencion)
+intervencion_results = execute_source_query(query_intervencion)
 
 
 query_prescribe = "SELECT * FROM PRESCRIBE;"
-prescribe_results = execute_query(query_prescribe)
+prescribe_results = execute_source_query(query_prescribe)
 
 
 query_practica = "SELECT * FROM PRACTICA;"
-practica_results = execute_query(query_practica)
+practica_results = execute_source_query(query_practica)
 
 
 query_realiza = "SELECT * FROM REALIZA;"
-realiza_results = execute_query(query_realiza)
+realiza_results = execute_source_query(query_realiza)
 
 
 query_ocupa = "SELECT * FROM OCUPA;"
-ocupa_results = execute_query(query_ocupa)
+ocupa_results = execute_source_query(query_ocupa)
 
 
 #ejemplo config Destino
